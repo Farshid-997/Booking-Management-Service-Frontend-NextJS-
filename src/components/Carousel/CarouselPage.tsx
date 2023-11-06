a321310b80d4140f93d753c99fee0d0e346a5e37
Farshid-997 <arnob.farshid@gmail.com>
2023-11-06 14:09:58 +0600

    add

--- src/components/Carousel/CarouselPage.tsx
+++ src/components/Carousel/CarouselPage.tsx
@@ -1,68 +1,26 @@
 "use client";
-import { Carousel } from "antd";
-import img1 from "../../assets/bannerimg.jpg";
-import img2 from "../../assets/bannerimg1.jpg";
-import img3 from "../../assets/bannerimg2.jpg";
+import { Col, Row } from "antd";
+import img1 from "../../assets/planner-shop-dress-1.png";
+
+import styles from "./styles.module.css";
 import Image from "next/image";
-const contentStyle: React.CSSProperties = {
-  color: "white",
-  position: "absolute",
-  top: "380px",
-  fontSize: "50px",
-  marginLeft: "10rem",
-  fontWeight: 600,
-};
 
-const contentStyle1: React.CSSProperties = {
-  color: "white",
-  position: "absolute",
-  top: "380px",
-  fontSize: "50px",
-  marginLeft: "10rem",
-  fontWeight: 600,
-};
-// const imageStyle = {
-//   width: "100%",
-//   height: "550px",
-// };
-
-const contentStyle2: React.CSSProperties = {
-  color: "white",
-  position: "absolute",
-  top: "380px",
-  fontSize: "50px",
-  marginLeft: "10rem",
-  fontWeight: 600,
-};
 const CarouselPage = () => (
-  <Carousel autoplay effect="fade" style={{ width: "100%", border: "none" }}>
-    <div>
-      <Image
-        src={img1}
-        alt="imag1 banner"
-        style={{ objectFit: "cover" }}
-      ></Image>
+  <Row gutter={[8, 8]}>
+    <Col xs={24} sm={12} md={6} xl={12}>
+      <p className={styles.para1}>Offeres are Coming!!</p>
+      <p className={styles.para}>
+        Making Your dream Long Lasting.We are Happy to see you here. We must
+        want that your dreamy wedding will made with us!
+      </p>
 
-      <h3 style={contentStyle}>Book Your Service Now!</h3>
-    </div>
-    <div>
-      <Image
-        src={img2}
-        alt="img2 banner"
-        style={{ objectFit: "cover" }}
-      ></Image>
+      <button className={styles.purchaseButton}>Purchase Now</button>
+    </Col>
 
-      <h3 style={contentStyle1}>Make your events memorable!</h3>
-    </div>
-    <div>
-      <Image
-        src={img3}
-        alt="img3 banner"
-        style={{ objectFit: "cover" }}
-      ></Image>
-      <h3 style={contentStyle2}>You can contact with us!</h3>
-    </div>
-  </Carousel>
+    <Col xs={24} sm={12} md={6} xl={12}>
+      <Image src={img1} alt="" className={styles.heroImage}></Image>
+    </Col>
+  </Row>
 );
 
 export default CarouselPage;
