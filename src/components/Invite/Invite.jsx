965e1e0d181149e8c6b78310b8bb9cc80d3b0bd2
Farshid Ahsan <arnob.farshid@gmail.com>
2023-11-22 02:33:58 +0600

    add

--- src/components/Invite/Invite.jsx
+++ src/components/Invite/Invite.jsx
@@ -12,31 +12,28 @@ export default function Invite() {
 
           <button className={styles.btnEvents}>The Reception</button>
           <p className={styles.eventsPara}>
-            Monday, 12 Apr. 2022  1:00 PM – 2:30 PM 4517 Washington
-            Ave. Manchester, Kentucky 39495 +1 234-567-8910 See
-            Location
+            Monday, 12 Apr. 2022 1:00 PM – 2:30 PM 4517 Washington Ave.
+            Manchester, Kentucky 39495 +1 234-567-8910 See Location
           </p>
         </Col>
 
         <Col xs={24} sm={12} md={12} lg={6}>
           <div className={styles.inviteDiv1}></div>
 
-          <button className={styles.btnEvents}>The Engagement</button>
-          <p className={styles.eventsPara}>
-            Monday, 12 Apr. 2022  1:00 PM – 2:30 PM 4517 Washington
-            Ave.  Manchester, Kentucky 39495 +1 234-567-8910 See
-            Location
+          <button className={styles.btnEvents1}>The Engagement</button>
+          <p className={styles.eventsPara1}>
+            Monday, 12 Apr. 2022 1:00 PM – 2:30 PM 4517 Washington Ave.
+            Manchester, Kentucky 39495 +1 234-567-8910 See Location
           </p>
         </Col>
 
         <Col xs={24} sm={12} md={12} lg={6}>
           <div className={styles.inviteDiv2}></div>
 
-          <button className={styles.btnEvents}>The Wedding</button>
-          <p className={styles.eventsPara}>
-            Monday, 12 Apr. 2022  1:00 PM – 2:30 PM 4517 Washington
-            Ave.  Manchester, Kentucky 39495 +1 234-567-8910 See
-            Location
+          <button className={styles.btnEvents2}>The Wedding</button>
+          <p className={styles.eventsPara2}>
+            Monday, 12 Apr. 2022 1:00 PM – 2:30 PM 4517 Washington Ave.
+            Manchester, Kentucky 39495 +1 234-567-8910 See Location
           </p>
         </Col>
       </Row>
